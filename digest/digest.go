05537a49caf1ef1f8e76ffb6a59ed03299f20dcb
Raja Srinivasan <rajasrinivasan@Rajas-MBP.fios-router.home>
2018-04-11 04:55:00 -0400

    added crc32

--- digest/digest.go
+++ digest/digest.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"flag"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"log"
 	"os"
@@ -14,6 +15,7 @@ import (
 var verbose bool
 var fmd5 bool
 var fsha256 bool
+var fcrc32 string
 var all bool
 var toplevel string
 
@@ -44,6 +46,27 @@ func GenerateDigest(fn string) {
 		}
 		fmt.Printf("%10s : %x\n", "SHA256", hsha256.Sum(nil))
 	}
+	f.Seek(0, 0)
+	if fcrc32 != "" {
+		var poltable *crc32.Table
+		switch fcrc32 {
+		case "IEEE":
+			poltable = crc32.MakeTable(crc32.IEEE)
+		case "Castagnoli":
+			poltable = crc32.MakeTable(crc32.Castagnoli)
+		case "Koopman":
+			poltable = crc32.MakeTable(crc32.Koopman)
+		default:
+			fmt.Println("Unknown CRC32 polynomial %s. Will use IEEE\n", fcrc32)
+			poltable = crc32.IEEETable
+		}
+		hcrc32 := crc32.New(poltable)
+		_, err := io.Copy(hcrc32, f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%10s : %x\n", "CRC32 "+fcrc32, hcrc32.Sum(nil))
+	}
 }
 
 func DigestAll(path string, info os.FileInfo, err error) error {
@@ -59,10 +82,12 @@ func main() {
 	flag.BoolVar(&fsha256, "sha256", false, "generate sha256 signature")
 	flag.BoolVar(&all, "all", false, "generate all signatures")
 	flag.StringVar(&toplevel, "walk", "", "walk this directory")
+	flag.StringVar(&fcrc32, "crc32", "", "crc32 IEEE|Castagnoli|Koopman")
 	flag.Parse()
 	if all {
 		fmd5 = true
 		fsha256 = true
+		fcrc32 = "IEEE"
 	}
 	if verbose {
 		if fmd5 {
@@ -70,6 +95,9 @@ func main() {
 		}
 		if fsha256 {
 			fmt.Println("SHA256 signature")
+		}
+		if fcrc32 != "" {
+			fmt.Println("CRC32 %s", fcrc32)
 		}
 		if toplevel != "" {
 			fmt.Println("Will walk the dir %s", toplevel)
