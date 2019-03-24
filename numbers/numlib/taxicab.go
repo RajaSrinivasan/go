f1ed42610345c1de5ab749ad0c3612fc3bd82abe
Srini <s@srin.me>
2019-03-24 06:56:45 -0400

    restructure library

---
+++ numbers/numlib/taxicab.go
@@ -0,0 +1,109 @@
+package numlib
+
+import (
+	"fmt"
+	"math"
+)
+
+type NumPair struct {
+	N1 int
+	N2 int
+}
+
+type SumCube struct {
+	Sum int
+	P1  NumPair
+	P2  NumPair
+}
+
+type TaxicabNumber struct {
+	SC1 SumCube
+	SC2 SumCube
+}
+
+func find(arr []SumCube, sum int) (bool, int) {
+	for idx, sc := range arr {
+		if sc.Sum == sum {
+			return true, idx
+		}
+	}
+	return false, 0
+}
+
+func TaxiCabNumbers(max int) []TaxicabNumber {
+	var sumCube SumCube
+	var tcNum TaxicabNumber
+	sumCubes := make([]SumCube, 1024)
+	taxCabs := make([]TaxicabNumber, 0)
+
+	maxNo := int(math.Pow(float64(math.MaxInt64), 1.0/3.0))
+	maxNo = 1000
+	for n1 := 1; n1 < maxNo && len(taxCabs) < max; n1++ {
+		n1cube := Cube(n1)
+		for n2 := 1; n2 < n1 && len(taxCabs) < max; n2++ {
+			n2cube := Cube(n2)
+			sumCube.Sum = n1cube + n2cube
+			sumCube.P1 = NumPair{n1, n1cube}
+			sumCube.P2 = NumPair{n2, n2cube}
+			status, idx := find(sumCubes, sumCube.Sum)
+			if status {
+				tcNum.SC1 = sumCubes[idx]
+				tcNum.SC2 = sumCube
+				taxCabs = append(taxCabs, tcNum)
+				//fmt.Printf("%5d) %10d ", len(taxCabs), sumCube.Sum)
+				//PrintTaxicabNumber(tcNum)
+			} else {
+				sumCubes = append(sumCubes, sumCube)
+			}
+		}
+	}
+	return taxCabs
+}
+
+func TaxiCabNumbersOrder(max int, order int) []TaxicabNumber {
+	var sumCube SumCube
+	var tcNum TaxicabNumber
+	sumCubes := make([]SumCube, 1024)
+	taxCabs := make([]TaxicabNumber, 0)
+
+	maxNo := int(math.Pow(float64(math.MaxInt64), 1.0/3.0))
+	maxNo = 1000
+	for n1 := 1; n1 < maxNo && len(taxCabs) < max; n1++ {
+		n1cube := Power(n1, order)
+		for n2 := 1; n2 < n1 && len(taxCabs) < max; n2++ {
+			n2cube := Power(n2, order)
+			sumCube.Sum = n1cube + n2cube
+			sumCube.P1 = NumPair{n1, n1cube}
+			sumCube.P2 = NumPair{n2, n2cube}
+			status, idx := find(sumCubes, sumCube.Sum)
+			if status {
+				tcNum.SC1 = sumCubes[idx]
+				tcNum.SC2 = sumCube
+				taxCabs = append(taxCabs, tcNum)
+				//fmt.Printf("%5d) %10d ", len(taxCabs), sumCube.Sum)
+				//PrintTaxicabNumber(tcNum)
+			} else {
+				sumCubes = append(sumCubes, sumCube)
+			}
+		}
+	}
+	return taxCabs
+}
+
+func showSumCube(sc SumCube) {
+	fmt.Printf(" : %10d , %10d | %10d , %10d || ", sc.P1.N1, sc.P1.N2, sc.P2.N1, sc.P2.N2)
+}
+
+func PrintTaxicabNumber(tcNum TaxicabNumber) {
+	//fmt.Printf("%10d = ", tcNum.SC1.Sum)
+	showSumCube(tcNum.SC1)
+	showSumCube(tcNum.SC2)
+	fmt.Println()
+}
+func PrintTaxicabNumbers(tc []TaxicabNumber) {
+	for idx, tcNum := range tc {
+		fmt.Printf("%4d) ", idx)
+		fmt.Printf("%10d || ", tcNum.SC1.Sum)
+		PrintTaxicabNumber(tcNum)
+	}
+}
---
+++ numbers/numlib/taxicab.md
@@ -0,0 +1,10 @@
+# Projectlet: Taxicab Numbers
+## Learning Objectives
+In this project, we will explore dynamically growing data structures, sorting and tradeoffs between time and space. 
+## Problem statement
+
+Another name for Hardy-Ramanujan numbers this projectlet approaches [taxicab numbers](https://www.wolframalpha.com/input/?i=taxicab+numbers) simplistically with a bruteforce search approach. We will try to answer these are sparse or there are many.
+
+We will extend the idea into taxicab numbers of a higher order ie to powers > 3 and try to answer similar questions.
+
+These explorations should also lead to an understanding of the time considerations in designing datastructures and algorithms.
